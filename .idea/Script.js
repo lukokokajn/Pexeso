6cabb72f283d9f4cf6b5cc332e98d34efb103dfc
lukokokajn <lukas.novy@educanet.cz>
2021-04-18 16:26:11 +0200

    DONE

--- .idea/Script.js
+++ .idea/Script.js
@@ -1,40 +1,40 @@
 let container;
-let pole1;
-let pole2;
+let cards;
+let cards1;
 
 let Function = function () {
     container = document.getElementById("container")
-    pole1 = ["😂", "🥰", "🤔", "🙂", "😍", "😎", "🤩", "😃"]
-    pole2 = pole1.length;
+    cards = ["😂", "🥰", "🤔", "🙂", "😍", "😎", "🤩", "😃"]
+    cards1 = cards.length;
 
-    Double(pole1);
-    Random(pole1);
+    Double(cards);
+    Random(cards);
     Pole();
 }
 
 let Double = function (item) {
-    for (let i = 0; i < pole2; i++) {
+    for (let i = 0; i < cards1; i++) {
         item.push(item[i])
     }
     return item;
 }
 
 let Random = function (item1) {
-    for (let j = 0; j < pole1.length; j++) {
+    for (let j = 0; j < cards.length; j++) {
         let randomator = Math.floor(Math.random() * 16);
-        let pole1Element = pole1[j];
+        let pole1Element = cards[j];
 
-        pole1[j] = pole1[randomator];
-        pole1[randomator] = pole1Element;
+        cards[j] = cards[randomator];
+        cards[randomator] = pole1Element;
     }
     return item1;
 }
 
 let Pole = function () {
-    for (let x = 0; x < pole1.length; x++) {
+    for (let x = 0; x < cards.length; x++) {
         let div = document.createElement('div')
 
-        div.innerText = pole1[x]
+        div.innerText = cards[x]
         container.appendChild(div)
     }
 }
